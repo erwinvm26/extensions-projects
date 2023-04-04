6053ad57a8ff0943e75258e911dbea3e7f927a78
erwinvm26 <erwinmayorga26@gmail.com>
2023-04-04 10:01:53 -0600

    add new extension named example-extension

---
+++ example-extension/manifest.ts
@@ -0,0 +1,37 @@
+import packageJson from "./package.json";
+
+const icons = {
+  16: "icons/document_16.png",
+  24: "icons/document_24.png",
+  32: "icons/document_32.png",
+  48: "icons/document_48.png",
+  128: "icons/document_128.png",
+  256: "icons/document_256.png",
+};
+
+export const manifest: chrome.runtime.ManifestV3 = {
+  manifest_version: 3,
+  version: packageJson.version,
+  name: packageJson.name,
+  description: packageJson.description,
+  author: packageJson.author.name,
+  icons: {
+    "16": icons[16],
+    "48": icons[48],
+    "128": icons[128],
+    "256": icons[256],
+  },
+  action: {
+    default_icon: {
+      "16": icons[16],
+      "24": icons[24],
+      "32": icons[32],
+    },
+    default_popup: "./src/pages/popup/index.html",
+  },
+  options_page: "./src/pages/options/index.html",
+  background: {
+    type: "module",
+    service_worker: "js/background.js",
+  },
+};
---
+++ weather-extension/src/config/index.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+
+export default {
+  VITE_ENV: process.env.VITE_ENV,
+  API_KEY: process.env.API_KEY,
+  URL_API: process.env.URL_API,
+};
