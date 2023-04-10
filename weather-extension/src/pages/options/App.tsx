cc7a914c63322772a18d81f5a588868fb321d848
erwinvm26 <erwinmayorga26@gmail.com>
2023-04-10 14:20:09 -0600

    [add] option pages

--- weather-extension/src/pages/options/App.tsx
+++ weather-extension/src/pages/options/App.tsx
@@ -1,34 +1,90 @@
-import { useState } from "react";
-import reactLogo from "@assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
+import {
+  Card,
+  CardBody,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  Button,
+} from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+import { setStorage, getStorage, getCity, WeatherApiData } from "@src/utils";
+
+interface Form {
+  cityName: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [qAlert, setQAlert] = useState<boolean>(false);
+  const [value, setValue] = useState("");
+  const { register, handleSubmit, reset } = useForm<Form>();
+
+  useEffect(() => {
+    const funTemp = async () => {
+      const response = await getStorage("values");
+
+      setValue(response.values ?? "");
+    };
+
+    funTemp();
+  }, []);
+
+  const onSubmit: SubmitHandler<Form> = async ({ cityName }) => {
+    try {
+      const dataCity = await getCity({
+        q: cityName,
+      });
+
+      if (!dataCity) {
+        throw new Error("City not found");
+      }
+
+      const result = [dataCity];
+
+      const store = await setStorage<WeatherApiData[]>({
+        data: result,
+        values: cityName,
+      });
+
+      if (store.values !== "") {
+        setQAlert(true);
+        setTimeout(() => {
+          setQAlert(false);
+        }, 1000);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Card width="500px">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <CardBody>
+          <FormControl>
+            <FormLabel>City Default</FormLabel>
+            <Input
+              type="text"
+              {...register("cityName", {
+                required: true,
+                pattern: /^[A-Za-z]+$/i,
+                maxLength: 100,
+                minLength: 3,
+              })}
+              value={value}
+              onChange={(prev) => setValue(prev.target.value)}
+              placeholder="Write to here"
+            />
+            {qAlert && <FormHelperText>It is saved</FormHelperText>}
+          </FormControl>
+          <Button type="submit" colorScheme="blue" width="full" mt="4">
+            Save
+          </Button>
+        </CardBody>
+      </form>
+    </Card>
   );
 }
 
--- weather-extension/src/utils/apiChrome.ts
+++ weather-extension/src/utils/apiChrome.ts
@@ -1,16 +1,28 @@
 interface Storage<T = any> {
   data?: T[];
-  values?: T;
+  values?: string;
 }
 
-export function setStorage<T = unknown>({ data }: Storage): Promise<T> {
+export function setStorage<T = unknown>({
+  data,
+  values,
+}: Storage): Promise<Storage<T>> {
   return new Promise((resolve) => {
-    chrome.storage.local.set({ data }).then(() => {
-      return resolve(data as T);
+    chrome.storage.local.set({ data, values }).then(() => {
+      return resolve({
+        data,
+        values,
+      });
     });
   });
 }
 
 export function getStorage<T = unknown>(key: keyof Storage) {
   return chrome.storage.local.get([key]) as Storage<T>;
+  // return chrome.storage.local.get([key]);
+}
+
+async function main() {
+  const res = await getStorage("values");
+  res.values;
 }
